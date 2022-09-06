625b2b1825b006d26b80e20989d5cbfc7d498870
d1nocan <cuyar2000@gmail.com>
2022-09-06 16:36:54 +0300

    Created app

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "palette": {
+          100: "#000000",
+          200: "#5c5edc",
+          300: "#634acd",
+          400: "#6a36be",
+          500: "#7123af",
+        }
+      },
+    },
+  },
+  plugins: [],
+};
