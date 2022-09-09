17d7b487142de6f2366b60c395498a5a71ae89d9
d1nocan <cuyar2000@gmail.com>
2022-09-09 22:02:50 +0300

    Add glowing background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,12 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex flex-col gap-10 text-center">
-      <h1 className="text-8xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-palette-200 to-palette-500">Welcome to DShop!</h1>
-    </div>
+    <div className="relative flex flex-col gap-10 text-center group">
+      <div className="glow group-hover:opacity-100 duration-300"></div>
+      <div className="relative bg-neutral-900 p-6 rounded-xl">
+        <h1 className="text-8xl font-extrabold text-transparent bg-clip-text primary-gradient cursor-default">Welcome to DShop!</h1>
+        </div>
+      </div>
   );
 };
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,6 +13,22 @@ module.exports = {
           500: "#7123af",
         }
       },
+      animation: {
+        tilt: "tilt 10s infinite linear"
+      },
+      keyframes: {
+        tilt: {
+          "0%, 50%, 100%":{
+            transform: "rotate(0deg)",
+          },
+          "25%": {
+            transform: "rotate(1deg)",
+          },
+          "75%": {
+            transform: "rotate(-1deg)",
+          },
+        }
+      }
     },
   },
   plugins: [],
