de6f82c8c8581f27b837f042991906d11d2679ee
d1nocan <cuyar2000@gmail.com>
2022-09-16 16:14:50 +0300

    Update sort classnames

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,18 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="relative flex flex-col gap-10 text-center group">
-      <div className="glow group-hover:opacity-100 duration-300"></div>
-      <div className="relative bg-neutral-900 p-6 rounded-xl">
-        <h1 className="text-8xl font-extrabold text-transparent bg-clip-text primary-gradient cursor-default">Welcome to DShop!</h1>
+    <>
+      <div className="flex flex-col max-w-4xl gap-10 my-auto">
+        <div className="relative flex flex-col text-center scale-75 group lg:scale-100">
+          <div className="duration-300 glow group-hover:opacity-100"></div>
+          <div className="relative p-6 dark:bg-neutral-900 bg-neutral-100 rounded-xl">
+            <h1 className="font-extrabold text-transparent cursor-default text-8xl bg-clip-text primary-gradient">
+              Welcome to DShop!
+            </h1>
+          </div>
         </div>
       </div>
+    </>
   );
 };
 
