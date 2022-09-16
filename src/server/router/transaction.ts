7b6a05a8b27dfc962d8b00b446d28f3b277e6901
d1nocan <cuyar2000@gmail.com>
2022-09-16 16:15:12 +0300

    Add transaction router

---
+++ src/server/router/transaction.ts
@@ -0,0 +1,36 @@
+import { Status } from '@prisma/client';
+import { selectTransaction, updateTransaction } from '@schemas/transaction';
+import { createProtectedRouter } from "./protected-router";
+
+export const transactionRouter = createProtectedRouter()
+    .query("get", {
+        resolve({ ctx }) {
+            return ctx.prisma.transaction.findMany();
+        }
+    })
+    .query("select", {
+        input: selectTransaction,
+        resolve({ ctx, input }) {
+            return ctx.prisma.transaction.findUnique({
+                where: {
+                    id: input.id,
+                },
+            });
+        }
+    })
+    .mutation("update", {
+        input: updateTransaction,
+        resolve({ ctx, input }) {
+            return ctx.prisma.transaction.update({
+                where: {
+                    id: input.id,
+                },
+                data: {
+                    id: input.id,
+                    status: input.status as Status,
+                },
+            });
+        }
+    });
+
+export default transactionRouter;
