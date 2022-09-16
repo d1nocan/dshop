0cada810427047759da886b6f9ec19e0d84e0c25
d1nocan <cuyar2000@gmail.com>
2022-09-16 23:22:50 +0300

    add theme-change

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,34 +2,8 @@
 module.exports = {
   darkMode: "class",
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        "palette": {
-          100: "#000000",
-          200: "#5c5edc",
-          300: "#634acd",
-          400: "#6a36be",
-          500: "#7123af",
-        }
-      },
-      animation: {
-        tilt: "tilt 10s infinite linear"
-      },
-      keyframes: {
-        tilt: {
-          "0%, 50%, 100%":{
-            transform: "rotate(0deg)",
-          },
-          "25%": {
-            transform: "rotate(1deg)",
-          },
-          "75%": {
-            transform: "rotate(-1deg)",
-          },
-        }
-      }
-    },
-  },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark"],
+  }
 };
