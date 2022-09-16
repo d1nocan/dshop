122da88ba7c9f52bc24b12796de08924068035d3
d1nocan <cuyar2000@gmail.com>
2022-09-16 16:18:02 +0300

    Add transaction router to appRouter

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -1,6 +1,7 @@
 // src/server/router/index.ts
 import { default as item } from './item';
 import { default as user } from './user';
+import { default as transaction } from './transaction';
 import { createRouter } from "./context";
 import superjson from "superjson";
 import {default as leaderboard} from './leaderboard';
@@ -9,6 +10,7 @@ export const appRouter = createRouter()
   .transformer(superjson)
   .merge("leaderboard.", leaderboard)
   .merge("item.", item)
+  .merge("transaction.", transaction)
   .merge("user.", user);
 
 // export type definition of API
