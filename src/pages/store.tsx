a2f28a2363cb82a1101a04b5157a362e28a0344a
d1nocan <cuyar2000@gmail.com>
2022-09-16 16:15:29 +0300

    Add store page

---
+++ src/pages/store.tsx
@@ -0,0 +1,25 @@
+import Card from "../components/card";
+import type { NextPage } from "next";
+import { trpc } from "src/utils/trpc";
+
+const Store: NextPage = () => {
+  const items = trpc.useQuery(["item.get"]);
+  return (
+    <>
+      <div className="container my-auto w-6/12 flex flex-row gap-4 justify-center flex-shrink-0 flex-wrap">
+        {items.data?.filter((item) => item.isHidden === false).map((item, index) =>
+             (
+                <Card
+                  key={index}
+                  description={item.description as string}
+                  title={item.name}
+                  price={item.price}
+                />
+            )
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Store;
