f7b1e6b9506850e948be3bfecc59e21beb034c29
d1nocan <cuyar2000@gmail.com>
2022-09-16 16:13:24 +0300

    Add navigation and update api

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,75 +1,73 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
+import Link from "next/link";
 import { trpc } from "src/utils/trpc";
 import dalle from "../../public/dalle.png";
 
 export default function Navbar() {
-  const { data: leaderboardData } = trpc.useQuery(["leaderboard.leaderboard"]);
+  const { data: leaderboardData } = trpc.useQuery(["leaderboard.get"]);
   const { data: session } = useSession();
   return (
     <>
       <nav className="navbar">
-        <div className="flex flex-col justify-around h-full">
-          <div className="profile">
-            <div className="card">
-              {session && (
-                <div className="profile">
-                  <Image
-                    src={session.user?.image || dalle}
-                    width={75}
-                    height={75}
-                    className="rounded-full"
-                    alt="profile"
-                  />
-                  <div className="my-auto">
-                    <p className="primary-text text-left">
-                      {session.user?.name}
-                    </p>
-                    <p className="secondary-text">
-                      {session.user?.points} points
-                    </p>
-                  </div>
+        <div className="profile mt-6">
+          <div className="card">
+            {session && (
+              <div className="profile">
+                <Image
+                  src={session.user?.image || dalle}
+                  width={75}
+                  height={75}
+                  className="rounded-full"
+                  alt="profile"
+                />
+                <div className="my-auto">
+                  <p className="primary-text text-left">{session.user?.name}</p>
+                  <p className="secondary-text">
+                    {session.user?.points} points
+                  </p>
                 </div>
-              )}
-              <button
-                  className="primary-button"
-                  type="button"
-                  onClick={session ? () => signOut() : () => signIn()}
-                >
-                  {session ? "Sign out" : "Sign in"}
-                </button>
               </div>
+            )}
+            <button
+              className="primary-button"
+              type="button"
+              onClick={session ? () => signOut() : () => signIn()}
+            >
+              {session ? "Sign out" : "Sign in"}
+            </button>
           </div>
-          <div className="leaderboard overflow-auto">
-              <p className="font-bold primary-text">
-                Leaderboard
-              </p>
-              <table className="table-auto mx-auto w-full">
-                <thead>
-                  <tr>
-                    <th className="w-1/2 py-2 primary-text">
-                      Name
-                    </th>
-                    <th className="w-1/2 py-2 primary-text">
-                      Points
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {leaderboardData?.map((user, index) => (
-                    <tr key={index}>
-                      <td className="py-2 secondary-text">
-                        {user.name}
-                      </td>
-                      <td className="py-2 secondary-text">
-                        {user.points}
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
+        </div>
+        <div className="nav">
+          <div className="card p-6 gap-2">
+            <Link href="/">
+              <a className="primary-text">Main</a>
+            </Link>
+            <Link href="/dashboard/users">
+              <a className="primary-text">Users</a>
+            </Link>
+            {session?.user?.role === "ADMIN" && (
+              <Link href="/dashboard/items">
+                <a className="primary-text">Items</a>
+              </Link>
+            )}
+            <Link href="/store">
+                <a className="primary-text">Store</a>
+              </Link>
+            <Link href="/dashboard/transactions">
+              <a className="primary-text">Transactions</a>
+            </Link>
+          </div>
+        </div>
+        <div className="leaderboard overflow-auto">
+          <p className="font-bold primary-text mb-4">Leaderboard</p>
+          {leaderboardData?.map((user, index) => (
+            <div key={index} className="flex flex-row justify-around my-2">
+              <p className="secondary-text break-all w-1/3">{user.name}</p>
+              <p className="secondary-text w-1/3">{user.points}</p>
             </div>
-          </div>
+          ))}
+        </div>
       </nav>
     </>
   );
