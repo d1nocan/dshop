6dc49c8c1a806ec28821db67c728a457a423a023
d1nocan <cuyar2000@gmail.com>
2022-09-17 14:39:29 +0300

    Update appRouter

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,6 +2,7 @@
 import { default as item } from './item';
 import { default as user } from './user';
 import { default as transaction } from './transaction';
+import { default as ticket } from "./ticket";
 import { createRouter } from "./context";
 import superjson from "superjson";
 import {default as leaderboard} from './leaderboard';
@@ -10,6 +11,7 @@ export const appRouter = createRouter()
   .transformer(superjson)
   .merge("leaderboard.", leaderboard)
   .merge("item.", item)
+  .merge("ticket.", ticket)
   .merge("transaction.", transaction)
   .merge("user.", user);
 
