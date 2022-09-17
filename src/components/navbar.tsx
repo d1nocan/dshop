3c24755d9d0a710b07d31caaf4d323aa59bfe24b
d1nocan <cuyar2000@gmail.com>
2022-09-17 14:16:44 +0300

    Update Navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,74 +1,113 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
-import { trpc } from "src/utils/trpc";
 import dalle from "../../public/dalle.png";
-
+import Moon from "../../public/moon.svg";
+import Sun from "../../public/sun.svg";
+import { useEffect } from "react";
+import { themeChange } from "theme-change";
 export default function Navbar() {
-  const { data: leaderboardData } = trpc.useQuery(["leaderboard.get"]);
   const { data: session } = useSession();
+  useEffect(() => {
+    themeChange(false);
+  }, []);
   return (
     <>
-      <nav className="navbar">
-        <div className="profile mt-6">
-          <div className="card">
-            {session && (
-              <div className="profile">
-                <Image
-                  src={session.user?.image || dalle}
-                  width={75}
-                  height={75}
-                  className="rounded-full"
-                  alt="profile"
-                />
-                <div className="my-auto">
-                  <p className="primary-text text-left">{session.user?.name}</p>
-                  <p className="secondary-text">
-                    {session.user?.points} points
-                  </p>
-                </div>
-              </div>
-            )}
-            <button
-              className="primary-button"
-              type="button"
-              onClick={session ? () => signOut() : () => signIn()}
+      <div className="navbar bg-neutral text-neutral-content w-11/12 mx-auto rounded-b-xl">
+        <div className="flex-1 gap-3">
+        <div className="dropdown dropdown-hover">
+          <button tabIndex={10} type="button" title="Links" className="btn btn-square btn-ghost">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block w-5 h-5 stroke-current"
             >
-              {session ? "Sign out" : "Sign in"}
-            </button>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          </button>
+          <ul tabIndex={0} className="p-2 gap-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+            <li><Link href="/dashboard/users"><a>Users</a></Link></li>
+            <li><Link href="/dashboard/items"><a>Items</a></Link></li>
+            <li><Link href="/store"><a>Store</a></Link></li>
+            <li><Link href="/dashboard/transactions"><a>Transactions</a></Link></li>
+          </ul>
+          </div>
+          <label className="swap swap-rotate">
+            <input
+              type="checkbox"
+              title="theme"
+              data-toggle-theme="dark,light"
+            />
+
+            <Sun
+              className="swap-on fill-current w-10 h-10 pt-3"
+              data-set-theme="dark"
+              data-act-class="ACTIVECLASS"
+            />
+
+            <Moon
+              className="swap-off fill-current w-10 h-10 pt-3"
+              data-set-theme="light"
+              data-act-class="ACTIVECLASS"
+            />
+          </label>
+        </div>
+        <div className="flex-1">
+          <div className="btn btn-ghost text-xl">
+            <Link href="/"><span>DShop</span></Link>
           </div>
         </div>
-        <div className="nav">
-          <div className="card p-6 gap-2">
-            <Link href="/">
-              <a className="primary-text">Main</a>
-            </Link>
-            <Link href="/dashboard/users">
-              <a className="primary-text">Users</a>
-            </Link>
-            {session?.user?.role === "ADMIN" && (
-              <Link href="/dashboard/items">
-                <a className="primary-text">Items</a>
-              </Link>
-            )}
-            <Link href="/store">
-                <a className="primary-text">Store</a>
-              </Link>
-            <Link href="/dashboard/transactions">
-              <a className="primary-text">Transactions</a>
-            </Link>
+        {session?.user ? (
+          <div className="dropdown dropdown-end pr-2">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-8">
+                <Image
+                  alt={session?.user?.name as string}
+                  src={session?.user?.image as string}
+                  layout="fill"
+                  className="rounded-full"
+                />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="mt-3 p-2 gap-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <p className="mx-auto">{session.user.name}</p>
+              </li>
+              <li>
+                <span className="badge mx-auto">
+                  {session.user.points} Points
+                </span>
+              </li>
+              <li>
+                <button
+                  type="button"
+                  className="mx-auto"
+                  onClick={() => signOut()}
+                >
+                  Logout
+                </button>
+              </li>
+            </ul>
           </div>
-        </div>
-        <div className="leaderboard overflow-auto">
-          <p className="font-bold primary-text mb-4">Leaderboard</p>
-          {leaderboardData?.map((user, index) => (
-            <div key={index} className="flex flex-row justify-around my-2">
-              <p className="secondary-text break-all w-1/3">{user.name}</p>
-              <p className="secondary-text w-1/3">{user.points}</p>
-            </div>
-          ))}
-        </div>
-      </nav>
+        ) : (
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={() => signIn()}
+          >
+            Login
+          </button>
+        )}
+      </div>
     </>
   );
 }
