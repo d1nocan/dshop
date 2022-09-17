e91863496b98a4b373dc2f0d708f8c439885d337
d1nocan <cuyar2000@gmail.com>
2022-09-17 14:16:31 +0300

    Update layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,29 +1,15 @@
-import React, { useEffect, useState } from "react";
-import Moon from "../../public/moon.svg";
-import Sun from "../../public/sun.svg";
+import React from "react";
 import Navbar from "./navbar";
 type layoutProps = {
     children: React.ReactNode;
 }
+
 export default function Layout({children} : layoutProps){
-  const [theme, setTheme] = useState("");
-  useEffect(() => {
-    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      setTheme("dark");
-    }
-  }, []);
-  useEffect(() => {
-    theme === "dark"
-      ? document.documentElement.classList.add("dark")
-      : document.documentElement.classList.remove("dark");
-  }, [theme]);
-    return(<div className="flex flex-col overflow-auto bg-neutral-100 dark:bg-neutral-900">
+    return(
+    <>
       <Navbar />
-        <button type="button" className="dark:text-neutral-50 text-neutral-900 absolute p-4 z-50 scale-90" onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-            {theme === "dark" ? <Sun /> : <Moon />}
-        </button>
-      <div className="children">
+      <div className="bg-base-200">
         {children}
       </div>
-    </div>)
+    </>)
 }
