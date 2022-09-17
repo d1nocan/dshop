81b83ae4dfa6c37fe9a9a5dd96322aff92f599a2
d1nocan <cuyar2000@gmail.com>
2022-09-17 14:16:14 +0300

    Update card component

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -12,22 +12,17 @@ type CardProps = {
 
 const Card = ({title, description, image, price, isHidden} : CardProps) => {
   return (
-    <div className="flex flex-col justify-between w-4/12 overflow-auto shadow-2xl rounded-xl bg-neutral-100 dark:bg-neutral-800">
-      <figure className="relative w-full h-48">
-        <Image src={image || dalle} layout="fill" alt="product" />
-      </figure>
-      <div className="relative pt-4 pb-6 pl-4 my-auto overflow-hidden mb-7">
-        <h1 className="mb-1 text-lg font-bold text-neutral-900 dark:text-neutral-100">
-            {title}
-        </h1>
-        <p className="inline-block px-2 text-sm font-light align-middle border border-neutral-900 dark:border-neutral-50 rounded-xl text-neutral-900 dark:text-neutral-100">{price} Points</p>
-        <p className="mt-2 break-words text-neutral-700 dark:text-neutral-100">
-          {description}
-        </p>
-        {isHidden && (<p className="absolute mb-2 text-red-600">Gizli</p>)}
-      </div>
-        <button type="button" className="w-full py-2 font-semibold rounded-md bg-palette-200 text-neutral-100">Details</button>
+    <div className="card w-64 bg-base-100 shadow-xl">
+  <figure className="relative w-full h-48"><Image src={image || dalle} layout="fill" alt="product" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">{title}{isHidden && (<p className="text-sm my-auto text-red-600">Gizli</p>)}</h2>
+    <p className="truncate">{description}</p>
+    <div className="card-actions justify-end">
+      <p className="my-auto">{price} Points</p>
+      {isHidden === undefined ? (<button type="button" className="btn btn-primary">Details</button>) : (<button type="button" className="btn btn-primary">Edit</button>)}
     </div>
+  </div>
+</div>
   );
 };
 
