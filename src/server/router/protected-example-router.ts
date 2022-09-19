10187120089aeced54450830ad27cdcc18a32997
d1nocan <cuyar2000@gmail.com>
2022-09-19 15:11:50 +0300

    Remove unused router

--- src/server/router/protected-example-router.ts
+++
@@ -1,9 +0,0 @@
-import { createProtectedRouter } from "./protected-router";
-
-// Example router with queries that can only be hit if the user requesting is signed in
-export const protectedExampleRouter = createProtectedRouter()
-  .query("getSession", {
-    resolve({ ctx }) {
-      return ctx.session;
-    },
-  });
