ecbe446748b047418db12e19cf11a68fb16e7bce
d1nocan <cuyar2000@gmail.com>
2022-09-23 20:39:35 +0300

    Add supabase image upload

---
+++ src/utils/supabase.ts
@@ -0,0 +1,17 @@
+import { createClient } from "@supabase/supabase-js";
+import { v4 as uuidv4 } from "uuid";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON as string;
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+const uploadImage = async (files: FileList) => {
+    const file = files[0] as File;
+    const name = `${uuidv4().toString()}.${file.type.split("/")[1]}`;
+    await supabase.storage
+        .from("dshop-images")
+        .upload(name, file);
+    return `${supabaseUrl}/storage/v1/object/public/dshop-images/${name}`;
+};
+
+export default uploadImage;
