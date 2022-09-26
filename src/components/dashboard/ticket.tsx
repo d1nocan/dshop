13c357c7cd76d3e4f18b883e428ac49f78c46080
d1nocan <cuyar2000@gmail.com>
2022-09-26 17:45:53 +0300

    update ticket component

--- src/components/dashboard/ticket.tsx
+++ src/components/dashboard/ticket.tsx
@@ -1,34 +1,35 @@
 import Image from "next/image";
-type Props = {
-    image: string | null;
-    name: string | null;
-    title: string | null;
-    createdAt: Date | null;
-    status: string | null;
-}
+import { Ticket, User } from "@prisma/client";
+import Link from "next/link";
 
-const Ticket = ({image, name, title, createdAt, status} : Props) => {
+type Props = {
+  index: number;
+  ticket: Ticket & { user: User };
+};
+
+const Ticket = ({index, ticket}: Props) => {
     return (<>
     <tr>
+        <td>{index}</td>
         <td>
           <div className="flex items-center space-x-3">
             <div className="avatar">
               <div className="mask mask-squircle w-12 h-12">
-                <Image src={image as string} alt={name as string} />
+                <Image src={ticket.user.image as string} alt={ticket.user.name as string} layout="fill"/>
               </div>
             </div>
             <div>
-              <div className="font-bold">{name}</div>
-              <div className="text-sm opacity-50">{createdAt?.toLocaleString()}</div>
+              <div className="font-bold">{ticket.user.name}</div>
+              <div className="text-sm opacity-50">{ticket.createdAt?.toLocaleString()}</div>
             </div>
           </div>
         </td>
         <td>
-          {title}
+          {ticket.title}
         </td>
-        <td>{status}</td>
+        <td>{ticket.status}</td>
         <th>
-          <button type="button" className="btn btn-ghost btn-xs">details</button>
+          <Link href={`./tickets/${ticket.id}`}><button type="button" className="btn btn-ghost btn-xs">details</button></Link>
         </th>
       </tr>
     </>)
