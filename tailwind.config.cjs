1b53279d49f43eca3d553168a4b8ae08aa1a5830
d1nocan <cuyar2000@gmail.com>
2022-09-28 11:21:03 +0300

    remove daisyui, theme-change add headlessui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,8 +2,5 @@
 module.exports = {
     darkMode: "class",
     content: ["./src/**/*.{js,ts,jsx,tsx}"],
-    plugins: [require("daisyui")],
-    daisyui: {
-        themes: ["light", "dark"],
-    },
+    plugins: [],
 };
