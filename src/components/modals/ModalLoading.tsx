0b5449ae9e357f147369d79d26191edf1eec0a5b
d1nocan <cuyar2000@gmail.com>
2022-10-08 13:51:21 +0300

    Prettierize

--- src/components/modals/ModalLoading.tsx
+++ src/components/modals/ModalLoading.tsx
@@ -1,6 +1,6 @@
 const ModalLoading = () => {
     return (
-        <div className="absolute z-50 -m-6 flex h-full w-full bg-neutral-100 dark:bg-neutral-800 opacity-100 duration-300">
+        <div className="absolute z-50 -m-6 flex h-full w-full bg-neutral-100 opacity-100 duration-300 dark:bg-neutral-800">
             <div className="m-auto aspect-square w-[5vh] animate-spin rounded-full border-2 border-b-0 border-l-0 border-neutral-900 dark:border-neutral-100"></div>
         </div>
     );
