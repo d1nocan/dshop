9f1782e6ef44cbe3d771ace30c8cc6a4b2d80d05
d1nocan <cuyar2000@gmail.com>
2022-10-13 14:16:40 +0300

    update imports

--- src/components/general/nextlink.tsx
+++ src/components/general/nextlink.tsx
@@ -1,11 +1,12 @@
-import Link, { LinkProps } from "next/link";
+import Link from "next/link";
+import type { LinkProps } from "next/link";
 import { forwardRef, HTMLProps } from "react";
 
 // eslint-disable-next-line react/display-name
 const NextLink = forwardRef<HTMLAnchorElement, LinkProps & HTMLProps<HTMLAnchorElement>>(
     ({ href, children, ...rest }, ref) => {
         return (
-            <Link href={href}>
+            <Link href={href} prefetch={false}>
                 <a ref={ref} {...rest}>
                     {children}
                 </a>
