1779212d00cc5d4c61baf81019a1027aa7705233
d1nocan <cuyar2000@gmail.com>
2022-10-18 16:01:35 +0300

    Change twitch iframe to link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import { env } from "src/env/client.mjs";
-import { getBaseUrl } from "@utils/trpc";
+import { TwitchLogo } from "phosphor-react";
+import Link from "next/link";
 const Home: NextPage = () => {
     return (
         <section>
@@ -9,16 +10,13 @@ const Home: NextPage = () => {
                     <h1 className="select-none bg-gradient-to-r from-purple-200 via-purple-400 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
                         {env.NEXT_PUBLIC_DEFAULT_SHOP_NAME}
                     </h1>
-                </div>
-                <div className="mx-auto mt-4 w-max lg:mt-0">
-                    <iframe
-                        title="Twitch stream"
-                        src={`https://player.twitch.tv/?channel=${env.NEXT_PUBLIC_TWITCH_CHANNEL}&parent=${
-                            getBaseUrl().split("//")[1]?.split(":")[0]
-                        }&muted=true`}
-                        allowFullScreen={true}
-                        className="aspect-video w-full rounded-lg border-2 border-violet-500 border-opacity-30 lg:w-[40vw]"
-                    />
+                    <Link href={`https://twitch.tv/${env.NEXT_PUBLIC_TWITCH_CHANNEL}`}>
+                        <TwitchLogo
+                            size={44}
+                            weight="light"
+                            className="mx-auto my-4 text-neutral-900 duration-300 hover:cursor-pointer hover:text-violet-300 dark:text-neutral-50 dark:hover:cursor-pointer dark:hover:text-violet-300"
+                        />
+                    </Link>
                 </div>
             </div>
         </section>
--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -3,7 +3,7 @@ import { createTRPCNext } from "@trpc/next";
 import type { AppRouter } from "../server/trpc/router/_app";
 import superjson from "superjson";
 
-export const getBaseUrl = () => {
+const getBaseUrl = () => {
     if (typeof window !== "undefined") return ""; // browser should use relative url
     if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
     return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
