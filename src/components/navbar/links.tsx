464c7ef59c2499fb9871ea8e0cfc7e67126907d6
d1nocan <cuyar2000@gmail.com>
2022-11-03 15:00:54 +0300

    Refactor Links component in navbar

--- src/components/navbar/links.tsx
+++ src/components/navbar/links.tsx
@@ -1,61 +1,69 @@
-import { Menu, Transition } from "@headlessui/react";
 import { type Role } from "@prisma/client";
+import { Content, Indicator, Item, List, Root, Trigger, Viewport } from "@radix-ui/react-navigation-menu";
 import { links } from "@utils/links";
 import { useSession } from "next-auth/react";
-import { Fragment } from "react";
-import { List, Placeholder } from "phosphor-react";
-import dynamic from "next/dynamic";
+import Link from "next/link";
+import { CaretDown } from "phosphor-react";
 
-const NextLink = dynamic(() => import("@general/nextlink"));
 export const Links = () => {
     const { data: session } = useSession();
     return (
-        <>
-            <Menu>
-                <Menu.Button
-                    aria-label="Links"
-                    as="button"
-                    className="text-neutral-content hover:bg-neutral-focus flex flex-row items-center justify-center rounded-lg py-2 px-4 text-sm font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
-                >
-                    <List size={22} color="white" weight="bold" />
-                </Menu.Button>
-                <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                >
-                    <Menu.Items
-                        as="nav"
-                        className="absolute z-10 mt-10 w-40 divide-y divide-neutral-800 divide-opacity-30 rounded-2xl bg-neutral-200 p-4 capitalize text-neutral-900 dark:divide-neutral-200 dark:divide-opacity-30 dark:bg-neutral-700 dark:text-neutral-100"
-                    >
-                        {links.map((link) => {
-                            if (link.roles && !link.roles.includes(session?.user?.role as Role)) {
-                                return null;
-                            }
-                            return (
-                                <Menu.Item key={link.href} as="div" className="flex flex-row">
-                                    {link.logo ? (
-                                        <link.logo className="my-auto text-xl" />
-                                    ) : (
-                                        <Placeholder className="my-auto text-xl" />
+        <Root className="relative flex w-fit z-[1] mx-auto">
+            <List className="flex p-2 rounded-md list-none">
+                {links.map((part) =>
+                    part.links.length > 1 ? (
+                        <Item key={part.title}>
+                            <Trigger className="flex duration-200 items-center group text-violet-500 focus:relative focus:shadow-sm hover:bg-neutral-200 dark:hover:bg-neutral-700 justify-between gap-1 py-2 px-3 outline-none select-none rounded text-base font-medium leading-none">
+                                {part.title}
+                                <CaretDown
+                                    className="relative text-violet-500 top-0 transform group-data-[state=open]:-rotate-180 motion-safe:transition motion-safe:transform motion-safe:duration-200 motion-safe:ease-linear"
+                                    aria-hidden
+                                />
+                            </Trigger>
+                            <Content className="absolute w-fit rounded-md border border-neutral-200 dark:border-neutral-700 dark:border-opacity-40 motion-safe:data-[motion='from-start']:animate-enterFromLeft motion-safe:data-[motion='from-end']:animate-enterFromRight motion-safe:data-[motion='to-start']:animate-exitToLeft motion-safe:data-[motion='to-end']:animate-exitToRight">
+                                <ul className="flex flex-col p-6 gap-x-3 list-none w-full">
+                                    {part.links.map(
+                                        (link) =>
+                                            (link.roles.includes(session?.user?.role as Role) ||
+                                                link.roles.length == 0) && (
+                                                <li>
+                                                    <Link
+                                                        className="py-2 px-3 outline-none select-none font-medium text-[15px] text-violet-500 focus:relative focus:shadow-sm p-3 rounded-md hover:bg-violet-500 hover:bg-opacity-10 block no-underline text-base leading-none"
+                                                        href={link.href}
+                                                    >
+                                                        <div
+                                                            id="title"
+                                                            className="font-medium leading-tight mb-2 text-violet-500"
+                                                        >
+                                                            {link.name}
+                                                        </div>
+                                                        {/*<p id="text" className="text-violet-400 leading-normal">{description}</p>*/}
+                                                    </Link>
+                                                </li>
+                                            ),
                                     )}
-                                    <NextLink
-                                        href={link.href}
-                                        className="group flex w-full items-center rounded-md py-2 px-2 text-sm capitalize"
-                                    >
-                                        {link.name}
-                                    </NextLink>
-                                </Menu.Item>
-                            );
-                        })}
-                    </Menu.Items>
-                </Transition>
-            </Menu>
-        </>
+                                </ul>
+                            </Content>
+                        </Item>
+                    ) : (
+                        <Item key={part.title}>
+                            <Link
+                                className="flex duration-200 items-center group text-violet-500 focus:relative focus:shadow-sm hover:bg-neutral-200 dark:hover:bg-neutral-700 justify-between gap-1 py-2 px-3 outline-none select-none rounded text-base font-medium leading-none"
+                                href={part.links[0]?.href as string}
+                            >
+                                {part.links[0]?.name}
+                            </Link>
+                        </Item>
+                    ),
+                )}
+                <Indicator className="flex items-end justify-center h-[10px] top-full overflow-hidden z-10 transition-transform motion-safe:data-[state='visible']:animate-fadeIn motion-safe:data-[state='hidden']:animate-fadeOut">
+                    <div className="relative top-[70%] bg-neutral-50 w-3 h-3 transform rotate-45 rounded-tl-sm" />
+                </Indicator>
+            </List>
+            <div className="absolute flex justify-center w-full top-full -left-12">
+                <Viewport className="relative mt-3 w-full bg-neutral-50 dark:bg-neutral-900 rounded-md overflow-hidden h-[var(--radix-navigation-menu-viewport-height)] sm:w-[var(--radix-navigation-menu-viewport-width)] motion-safe:transition-[width,_height] motion-safe:duration-300 motion-safe:ease-in-out motion-safe:data-[state='open']:animate-scaleIn motion-safe:data-[state='closed']:animate-scaleOut" />
+            </div>
+        </Root>
     );
 };
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,4 +3,96 @@ module.exports = {
     darkMode: "class",
     content: ["./src/**/*.{js,ts,jsx,tsx}"],
     plugins: [require("@tailwindcss/forms")],
+    theme: {
+        extend: {
+            keyframes: {
+                enterFromRight: {
+                    "0%": {
+                        transform: "translateX(200px)",
+                        opacity: "0",
+                    },
+                    "100%": {
+                        transform: "translateX(0px)",
+                        opacity: "1",
+                    },
+                },
+                enterFromLeft: {
+                    "0%": {
+                        transform: "translateX(-200px)",
+                        opacity: "0",
+                    },
+                    "100%": {
+                        transform: "translateX(0px)",
+                        opacity: "1",
+                    },
+                },
+                exitToRight: {
+                    "0%": {
+                        transform: "translateX(0px)",
+                        opacity: "1",
+                    },
+                    "100%": {
+                        transform: "translateX(200px)",
+                        opacity: "0",
+                    },
+                },
+                exitToLeft: {
+                    "0%": {
+                        transform: "translateX(0px)",
+                        opacity: "1",
+                    },
+                    "100%": {
+                        transform: "translateX(-200px)",
+                        opacity: "0",
+                    },
+                },
+                scaleIn: {
+                    "0%": {
+                        transform: "rotateX(-30deg) scale(0.9)",
+                        opacity: "0",
+                    },
+                    "100%": {
+                        transform: "rotateX(0deg) scale(1)",
+                        opacity: "1",
+                    },
+                },
+                scaleOut: {
+                    "0%": {
+                        transform: "rotateX(0deg) scale(1)",
+                        opacity: "1",
+                    },
+                    "100%": {
+                        transform: "rotateX(-10deg) scale(0.95)",
+                        opacity: "0",
+                    },
+                },
+                fadeIn: {
+                    "0%": {
+                        opacity: "0",
+                    },
+                    "100%": {
+                        opacity: "1",
+                    },
+                },
+                fadeOut: {
+                    "0%": {
+                        opacity: "1",
+                    },
+                    "100%": {
+                        opacity: "0",
+                    },
+                },
+            },
+            animation: {
+                enterFromLeft: "enterFromLeft 0.25s ease",
+                enterFromRight: "enterFromRight 0.25s ease",
+                exitToLeft: "exitToLeft 0.25s ease",
+                exitToRight: "exitToRight 0.25s ease",
+                fadeIn: "fadeIn 0.2s ease",
+                fadeOut: "fadeOut 0.2s ease",
+                scaleIn: "scaleIn 0.2s ease",
+                scaleOut: "scaleOut 0.2s ease",
+            },
+        },
+    },
 };
