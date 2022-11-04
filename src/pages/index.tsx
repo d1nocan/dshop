9baa83d5096a2b5cc2abab68379881fed19e769e
d1nocan <cuyar2000@gmail.com>
2022-11-04 20:33:02 +0300

    Update index logo style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const Home: NextPage = () => {
     return (
         <section>
-            <div className="my-64 mx-auto max-w-screen-xl px-4 lg:flex lg:h-96 lg:items-center">
+            <div className="absolute left-1/3 right-1/3 top-0 flex h-full max-w-screen-xl items-center px-4">
                 <div className="mx-auto max-w-3xl text-center">
                     <h1 className="select-none bg-gradient-to-r from-purple-200 via-purple-400 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
                         {env.NEXT_PUBLIC_DEFAULT_SHOP_NAME}
