8e995bc85418558a82c65b17e0c2041bd2025f5f
d1nocan <cuyar2000@gmail.com>
2022-11-05 14:49:33 +0300

    Change FAQ session

--- src/components/general/faq.tsx
+++ src/components/general/faq.tsx
@@ -1,60 +1,79 @@
-import { Disclosure, Transition } from "@headlessui/react";
 import { CaretDown } from "phosphor-react";
+import { w } from "windstitch";
+import {
+    Root,
+    Item,
+    Header,
+    Trigger,
+    Content,
+    type AccordionTriggerProps,
+    type AccordionContentProps,
+} from "@radix-ui/react-accordion";
+import { forwardRef } from "react";
+
+const StyledAccordion = w(Root, {
+    className: "rounded space-y-2 w-1/4 bg-transparent mx-auto mt-10",
+    defaultProps: {
+        collapsible: true,
+    },
+});
+
+const StyledItem = w(Item, {
+    className:
+        "overflow-hidden mt-0.5 first:mt-0 first:rounded-t last:rounded-b focus-within:relative focus-within:z-[1] focus-within:shadow",
+});
+const StyledHeader = w(Header, {
+    className: "[all:_'unset'] flex",
+});
+const StyledTrigger = w(Trigger, {
+    className:
+        "[all:_'unset'] group bg-transparent rounded-md px-5 h-11 flex-1 flex gap-4 duration-300 items-center justify-between text-base border border-violet-500 leading-none text-violet-600 shadow data-[state='closed']:bg-neutral-50 data-[state='closed']:border-opacity-20 data-[state='open']:border-opacity-70 dark:data-[state='closed']:bg-neutral-900 data-[state='open']:bg-neutral-50 dark:data-[state='open']:bg-neutral-900 hover:bg-neutral-200",
+});
+const StyledContent = w(Content, {
+    className:
+        "overflow-hidden text-base text-neutral-900 dark:text-neutral-300 border-x last:border-b border-violet-500 border-opacity-20 bg-neutral-50 dark:bg-neutral-900 data-[state='open']:animate-slideDown data-[state='closed']:animate-slideUp",
+});
+const StyledContentText = w.div("py-3.5 px-5");
+const StyledCaret = w(CaretDown, {
+    className:
+        "text-violet-600 group-data-[state='open']:rotate-180 motion-safe:transform motion-safe:transition motion-safe:duration-200 motion-safe:ease-linear",
+});
+
+const AccordionTrigger = forwardRef<HTMLButtonElement, AccordionTriggerProps>(
+    ({ children, ...props }, forwardedRef) => (
+        <StyledHeader>
+            <StyledTrigger {...props} ref={forwardedRef}>
+                {children}
+                <StyledCaret aria-hidden />
+            </StyledTrigger>
+        </StyledHeader>
+    ),
+);
+AccordionTrigger.displayName = "AccordionTrigger";
+
+const AccordionContent = forwardRef<HTMLDivElement, AccordionContentProps>(({ children, ...props }, forwardedRef) => (
+    <StyledContent {...props} ref={forwardedRef}>
+        <StyledContentText>{children}</StyledContentText>
+    </StyledContent>
+));
+AccordionContent.displayName = "AccordionContent";
+
 export default function FAQ() {
     return (
-        <div className="w-full px-4 pt-16">
-            <div className="relative mx-auto w-full max-w-md rounded-2xl bg-white p-2 dark:bg-neutral-800">
-                <Disclosure>
-                    {({ open }) => (
-                        <>
-                            <Disclosure.Button className="flex w-full justify-between rounded-lg bg-neutral-50 px-4 py-2 text-left text-sm font-medium focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75 dark:bg-neutral-900 dark:text-neutral-100">
-                                <span>I bought a product. When will it reach me?</span>
-                                <span className={`${open ? "rotate-180" : ""} scale-75 duration-100`}>
-                                    <CaretDown weight="bold" size={16} />
-                                </span>
-                            </Disclosure.Button>
-                            <Transition
-                                enter="transition duration-100 ease-out"
-                                enterFrom="transform scale-y-0 opacity-0"
-                                enterTo="transform scale-y-100 opacity-100"
-                                leave="transition duration-75 ease-out"
-                                leaveFrom="transform scale-y-100 opacity-100"
-                                leaveTo="transform scale-y-0 opacity-0"
-                            >
-                                <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-neutral-300">
-                                    We try to deliver the products quickly. It may take up to 5 business days for the
-                                    product to reach you.
-                                </Disclosure.Panel>
-                            </Transition>
-                        </>
-                    )}
-                </Disclosure>
-                <Disclosure as="div" className="mt-2">
-                    {({ open }) => (
-                        <>
-                            <Disclosure.Button className="flex w-full justify-between rounded-lg bg-neutral-50 px-4 py-2 text-left text-sm font-medium focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75 dark:bg-neutral-900 dark:text-neutral-100">
-                                <span>How can I earn points?</span>
-                                <span className={`${open ? "rotate-180" : ""} scale-75 duration-100`}>
-                                    <CaretDown weight="bold" size={16} />
-                                </span>
-                            </Disclosure.Button>
-                            <Transition
-                                enter="transition duration-100 ease-out"
-                                enterFrom="transform scale-y-0 opacity-0"
-                                enterTo="transform scale-y-100 opacity-100"
-                                leave="transition duration-75 ease-out"
-                                leaveFrom="transform scale-y-100 opacity-100"
-                                leaveTo="transform scale-y-0 opacity-0"
-                            >
-                                <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-neutral-300">
-                                    You can earn points by watching live streams, redeeming codes or by joining any
-                                    event.
-                                </Disclosure.Panel>
-                            </Transition>
-                        </>
-                    )}
-                </Disclosure>
-            </div>
-        </div>
+        <StyledAccordion type="single" defaultValue="item-1">
+            <StyledItem value="item-1">
+                <AccordionTrigger>I bought a product. When will it reach me?</AccordionTrigger>
+                <AccordionContent>
+                    We try to deliver the products quickly. It may take up to 5 business days for the product to reach
+                    you.
+                </AccordionContent>
+            </StyledItem>
+            <StyledItem value="item-2">
+                <AccordionTrigger>How can I earn points?</AccordionTrigger>
+                <AccordionContent>
+                    You can earn points by watching live streams, redeeming codes or by joining any event.
+                </AccordionContent>
+            </StyledItem>
+        </StyledAccordion>
     );
 }
