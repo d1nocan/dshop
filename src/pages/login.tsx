3ed1378ffc5390b5c515fd1be118ef23912e85e8
d1nocan <cuyar2000@gmail.com>
2022-11-21 19:30:07 +0300

    Update login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -8,7 +8,7 @@ export default function SignIn({ providers }: { providers: Provider[] }) {
     session.data?.user && router.push("/");
     return (
         <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg bg-neutral-900 dark:bg-neutral-50">
-            <div className="mx-auto flex w-fit flex-col gap-6 rounded-lg border-4 border-neutral-900 bg-neutral-100 p-10  duration-100 hover:-translate-x-1 hover:-translate-y-1 dark:border-neutral-50 dark:bg-neutral-800">
+            <div className="mx-auto flex w-fit min-w-[250px] flex-col gap-6 rounded-lg border-4 border-neutral-900 bg-neutral-100 p-10 duration-100 hover:-translate-x-1 hover:-translate-y-1 dark:border-neutral-50 dark:bg-neutral-800">
                 {Object.values(providers).map((provider) => (
                     <button
                         key={provider.name}
