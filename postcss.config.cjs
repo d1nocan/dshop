52f590a3bdecd1d89a8cfd08426839016426ce2e
d1nocan <cuyar2000@gmail.com>
2022-12-02 14:21:28 +0300

    Remove import and nesting plugins

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,5 @@
 module.exports = {
     plugins: {
-        "postcss-import": {},
-        "tailwindcss/nesting": "postcss-nesting",
         tailwindcss: {},
         autoprefixer: {},
         ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
